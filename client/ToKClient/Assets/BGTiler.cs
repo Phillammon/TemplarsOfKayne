168c35a64407ca6539817e650c8f4639aa1f09b9
Phillammon <alistair@hippotigris.org.uk>
2016-05-13 17:20:55 +0100

    Hopefully we have escaped from git hell. This should work?

---
+++ client/ToKClient/Assets/BGTiler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BGTiler : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            int tilesize = 6;
+            GameObject[] prevtiles = GameObject.FindGameObjectsWithTag("bgtile");
+            for(var i = 0 ; i < prevtiles.Length ; i ++)
+            {
+                Destroy(prevtiles[i]);
+            }
+            #I am aware the above is horrible practise, but this is quick and dirty.
+            int h = Screen.height;
+            int w = Screen.width;
+            int top = (int) Math.Floor((Camera.main.transform.position.y-h/2)/tilesize) * tilesize;
+            int left = (int) Math.Floor((Camera.main.transform.position.x-w/2)/tilesize) * tilesize;
+            Debug.Log("Top is " + (top));
+            Debug.Log("Left is " + (left));
+            GameObject newtile;
+            for (var x = left; x < left + w; x += tilesize){
+                for (var y = top; y < top + h; y += tilesize){
+                    newtile = GameObject.Instantiate(Resources.Load("BGTile")) as GameObject;
+                    newtile.transform.position = new Vector3(x, y, 1);
+                }
+            }
+	}
+}
---
+++ client/ToKClient/Assets/CameraPos.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPos : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+        Vector3 ppos = GameObject.Find("Player").transform.position;
+        Camera.main.transform.position = new Vector3(ppos.x, ppos.y, Camera.main.transform.position.z);
+    }
+    // Update is called once per frame
+    void Update () {
+        Vector3 mousepos = Input.mousePosition;
+        float smoothing = 0.2f;
+        Vector3 playerpos = GameObject.Find("Player").transform.position;
+        mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+        Vector3 targetpos = Vector3.Lerp(playerpos, mousepos, 0.5f);
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, new Vector3(targetpos.x, targetpos.y, Camera.main.transform.position.z), smoothing);
+    }
+}
