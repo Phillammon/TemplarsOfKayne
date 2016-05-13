6ca58fe31e9dbeb3c55bf714e42b77757eafc0f4
Phillammon <alistair@hippotigris.org.uk>
2016-05-13 17:47:17 +0100

    Fixed background tiling. Next up, reading input.

--- client/ToKClient/Assets/BGTiler.cs
+++ client/ToKClient/Assets/BGTiler.cs
@@ -10,24 +10,22 @@ public class BGTiler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-            int tilesize = 6;
             GameObject[] prevtiles = GameObject.FindGameObjectsWithTag("bgtile");
             for(var i = 0 ; i < prevtiles.Length ; i ++)
             {
                 Destroy(prevtiles[i]);
             }
-            #I am aware the above is horrible practise, but this is quick and dirty.
-            int h = Screen.height;
-            int w = Screen.width;
-            int top = (int) Math.Floor((Camera.main.transform.position.y-h/2)/tilesize) * tilesize;
-            int left = (int) Math.Floor((Camera.main.transform.position.x-w/2)/tilesize) * tilesize;
-            Debug.Log("Top is " + (top));
-            Debug.Log("Left is " + (left));
+            //I am aware the above is horrible practise, but this is quick and dirty.
+            int tilesize = 6;
+            int top = (int) Math.Floor(Camera.main.ScreenToWorldPoint(new Vector3(0,0,0)).y/tilesize)*tilesize;
+            int left = (int) Math.Floor(Camera.main.ScreenToWorldPoint(new Vector3(0,0,0)).x/tilesize)*tilesize;
+            int bottom = (int) Math.Ceiling(Camera.main.ScreenToWorldPoint(new Vector3(0,Screen.height,0)).y/tilesize)*tilesize;
+            int right = (int) Math.Ceiling(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,0,0)).x/tilesize)*tilesize;
             GameObject newtile;
-            for (var x = left; x < left + w; x += tilesize){
-                for (var y = top; y < top + h; y += tilesize){
+            for (var x = left; x < right; x += tilesize){
+                for (var y = top; y < bottom; y += tilesize){
                     newtile = GameObject.Instantiate(Resources.Load("BGTile")) as GameObject;
-                    newtile.transform.position = new Vector3(x, y, 1);
+                    newtile.transform.position = new Vector3(x+tilesize/2, y+tilesize/2, 1);
                 }
             }
 	}
